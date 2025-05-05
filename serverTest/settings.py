aaec95ae248725e4a30499910f5a33161f7bbe94
unknown <sweetlullxbyyy@gmail.com>
2025-05-05 12:44:20 -0400

    Cambio de host

--- serverTest/settings.py
+++ serverTest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--nrb(@qs(p32lqu=s!ae5r-k_*!7wk17+_w*2#yc6$0&=mjq33
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
